8325744a69cb049a322366b9256d840541f26d25
AmritSingh2630 <sin11396@sheridancollege.ca>
2021-06-07 23:43:59 -0400

    code inserted to ask the user for card value and suit in main class

--- src/amrit/singh/s991632630/CardTrick.java
+++ src/amrit/singh/s991632630/CardTrick.java
@@ -1,4 +1,5 @@
 package amrit.singh.s991632630;
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
@@ -14,7 +15,7 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] magicHand = new Card[7];
-
+        Scanner sc = new Scanner(System.in);
         for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
@@ -22,6 +23,11 @@ public class CardTrick {
         }
 
         //insert code to ask the user for Card value and suit, create their card
+        System.out.print("Enter card value: ");
+        int val = sc.nextInt();
+        System.out.print("Enter suit: ");
+        String suit = sc.nextLine();
+       
         // and search magicHand here
         //Then report the result here
     }
